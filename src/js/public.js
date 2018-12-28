816e84318efe2f1043e5bcf7fa4eff1988adbfbb
chuyongwyx <1525401582@qq.com>
2018-12-28 18:17:27 +0800

    header中完成动画页面

--- src/js/public.js
+++ src/js/public.js
@@ -1,6 +1,6 @@
 require('!style-loader!css-loader!../css/public.min.css');
 /*语言栏切换页面*/
-$('.lang').hover(function(){
+(function(){$('.lang').hover(function(){
 	$('.lang').css('color','#ffb81c');
 },function(){
 	$('.lang').css('color','#000');
@@ -39,23 +39,156 @@ $('#register').hover(function(){
 	$('#register').css('text-decoration','none');
 })
 /*二级导航栏的ajax动态生成*/
+//<div id=""class="list2">
+//					 				<div class="kind">
+//					 				<h4>分类</h4>
+//					 				<dl>
+//					 					<dt><a href="">男士包袋</a></dt>
+//					 					<dd><a href="">钱包</a></dd>
+//					 					<dd><a href="">钱包</a></dd>
+//					 					<dd><a href="">钱包</a></dd>
+//					 				</dl>
+//					 				<dl>
+//					 					<dt>女士包袋</dt>
+//					 					<dd><a href="">钱包</a></dd>
+//					 					<dd><a href="">钱包</a></dd>
+//					 					<dd><a href="">钱包</a></dd>
+//					 				</dl>
+//					 				</div>
+//					 				<div class="brand">
+//					 					<h4>品牌</h4>
+//					 					<dl>
+//					 						<dd><a href="">钱包</a></dd>
+//					 						<dd><a href="">钱包</a></dd>
+//					 						<dd><a href="">钱包</a></dd>
+//					 					</dl>
+//					 					<dl>
+//					 						<dd><a href="">钱包</a></dd>
+//					 						<dd><a href="">钱包</a></dd>
+//					 						<dd><a href="">钱包</a></dd>
+//					 					</dl>
+//					 					<dl>
+//					 						<dd><a href="">钱包</a></dd>
+//					 						<dd><a href="">钱包</a></dd>
+//					 						<dd><a href="">钱包</a></dd>
+//					 					</dl>
+//					 				</div>
+//					 				
+//					 			</div>
 $.get('../json/public.json?t='+new Date().getTime(),function(str){
 			//操作str数据
-			console.log(str);
+			//console.log(str);
+			var arr1 =["主页"];
+			var arr2 =["主页"];
+			var arr3 =[];
+			for(var jsonli in str){
+			 	 	arr1.push(jsonli);
+					arr2.push(eval(str[jsonli]));
+			 }
+			
+		    console.log(arr1);
+		    console.log(arr2);
+		//取出li遍历li有多少个li
+		  for(var i=1,len=$('#list li').length;i<len;i++){
+		  		var  box= document.createElement('div');
+		  		box.className ='list2';
+		  		box.innerHTML='<div class="kind"><h4>分类</h4><dl></dl><dl></dl></div><div class="brand"><h4>品牌</h4><dl></dl><dl></dl><dl></dl></div>';
+		  		 $('#list li')[i].appendChild(box);
+		  		 
+		  		 
+		  if(arr1[i]==$('#list li')[i].firstElementChild.innerText){	
+		  			
+		  		//arr2[i][0]里面是分类中的东西
+		  		for(var j =0,len=arr2[i][0].length;j<len;j++){
+		  			if(j<Math.ceil(len/2)){
+		  				var ds = document.createElement('dd');
+		  				ds.innerHTML ='<a href="#">'+arr2[i][0][j]+'</a>';
+		  				$('#list li')[i].lastElementChild.firstElementChild.firstElementChild.nextElementSibling.appendChild(ds);
+		  			}
+		  		if(j>=Math.ceil(len/2)){
+		  				var ds2 = document.createElement('dd');
+		  				ds2.innerHTML ='<a href="#">'+arr2[i][0][j]+'</a>';
+		  				$('#list li')[i].lastElementChild.firstElementChild.lastElementChild.appendChild(ds2);
+		  			}
+		  		}
+		  		//arr2[i][1]里面是品牌的东西
+		  		for(var k=0,lens=arr2[i][1].length;k<lens;k++){
+		  				if(k<lens&&k>Math.floor(lens/3)*2){
+		  					var ds3 = document.createElement('dd');
+		  					ds3.innerHTML='<a href="#">'+arr2[i][1][k]+'</a>';
+		  					$('#list li')[i].lastElementChild.lastElementChild.lastElementChild.appendChild(ds3);
+		  					
+		  				}
+		  				if(k<=Math.floor(lens/3)*2 && k>Math.floor(lens/3)){
+		  					var ds4 = document.createElement('dd');
+		  					ds4.innerHTML='<a href="#">'+arr2[i][1][k]+'</a>';
+		  			$('#list li')[i].lastElementChild.lastElementChild.lastElementChild.previousElementSibling.appendChild(ds4);		
+		  				}
+		  				if(k<=Math.floor(lens/3)){
+		  					var ds5 = document.createElement('dd');
+		  					ds5.innerHTML='<a href="#">'+arr2[i][1][k]+'</a>';
+		  					$('#list li')[i].lastElementChild.lastElementChild.firstElementChild.nextElementSibling.appendChild(ds5);
+		  				}
+		  				
+		  			
+		  			
+		  		}
+		  	}
+		  		
+		  		
+		  		
+		  }
+	
+		  
+	
+		  
+		
 })
 
 
 
 
 /*导航部分*/
+var list = document.getElementById('list');
+	var lis = list.getElementsByTagName('li');
 $('#list li').each(function(){
 		$(this).hover(function(){
-			$(this).children().css('color','#fff');;
-			//$(this).children().css('display','block');
+			
+			$(this)[0].firstElementChild.style.color='#fff';
+			if($(this)[0].lastElementChild.className=='list2'){
+			$(this)[0].firstElementChild.nextElementSibling.style.display='block';
+			$(this)[0].lastElementChild.style.display='block';
+			}
 				
 		},function(){
-			$(this).children().css('color','#ffb81c');
-			//$(this)[0].lastChild.style.display ='none';
-			//$('#first').css('display','block');
+			$(this)[0].firstElementChild.style.color='#ffb81c';
+			if($(this)[0].lastElementChild.className=='list2'){
+			$(this)[0].lastElementChild.previousElementSibling.style.display='none';
+			$(this)[0].lastElementChild.style.display='none';
+			}
 		})
+});
+
+//搜索框
+$('.s1').click(function(){
+	
+	$('#search_span').css('display','none');
+	$('#search_box').css('display','block');
+	$('#search_box').animate({'width':'200px'},1000);
+});
+$('.rspan').click(function(){
+		$('#search_span').css('display','none');
+	$('#search_box').css('display','block');
+	$('#search_box').animate({'width':'200px'},1000);
+});
+
+$('#search_btn').blur(function(){
+	console.log('hahah');
+	$('#search_box').animate({'width':'0'},1000,function(){
+		$('#search_box').css('display','none');
+	$('#search_span').css('display','block');
+	});
+	
 })
+
+})()
